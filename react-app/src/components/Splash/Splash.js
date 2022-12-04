9ea1822e477d0019d71244d1fbf5bad04e4c9738
Connor Lam <connorlam03@yahoo.com>
2022-12-04 15:12:24 -0800

    rought draft of splash page

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -1,15 +1,51 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 import './Splash.css'
+
+import splash1 from '../../assets/gba_photos/gba_splash.jpeg'
+import splash2 from '../../assets/gba_photos/gba_splash2.jpeg'
+import splash3 from '../../assets/gba_photos/gba_splash4.jpeg'
+
+const images = [
+  splash1,
+  splash2,
+  splash3
+];
 
 const Splash = () => {
 
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const [splashNum, setSplashNum] = useState(0);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const splashInterval = setInterval(() => {
+            setSplashNum((prevNum) => ++prevNum % images.length)
+        }, 5000)
+
+        return (() => {
+            clearInterval(splashInterval)
+        })
+    }, [])
+
     return (
-        <div>
-            Splash Page
-        </div>
-    )
+      <div
+        id="splash"
+        className="splash-main"
+        style={{
+          backgroundImage: `url(${images[splashNum]})`,
+        }}
+      >
+        {/* <img src={images[splashNum]} alt=''/> */}
+        {/* <div>
+          <h1>Global Badminton Academy</h1>
+        </div> */}
+      </div>
+    );
 }
 
 
