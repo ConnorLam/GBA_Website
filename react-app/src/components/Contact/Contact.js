98eddf13b9bade2f3f50895e8085c5c6f443b5bb
Connor Lam <connorlam03@yahoo.com>
2022-12-05 14:47:41 -0800

    contact us page rough draft

--- react-app/src/components/Contact/Contact.js
+++ react-app/src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import './Contact.css'
+import arena from '../../assets/gba_photos/Arena.webp'
 
 
 const Contact = () => {
@@ -10,6 +11,7 @@ const Contact = () => {
             </div>
             <div className='contact-body'>
                 <div className='form-div'>
+                    <h2 className='white email-us'>Email Us</h2>
                     <form action="https://formsubmit.co/connorlam03@gmail.com" method="POST">
                         <input type="hidden" name="_captcha" value="false"></input>
                         <input type="hidden" name="_subject" value="GBA website"></input>
@@ -22,10 +24,34 @@ const Contact = () => {
                         </div>
                     </form>
                 </div>
+                <div className='spacer'></div>
                 <div className='other-info-div'>
-                    <a target='_blank'>
-                        <i class="fa-brands fa-instagram fa-3x white"></i>
-                    </a>
+                    <h2 className='white other-info'>Other Info</h2>
+                    <div className='insta-facebook'>
+                        <div className='actual-link'>
+                            <a className='white navlink contact-links' rel='noreferrer' target='_blank' href='https://www.instagram.com/globalbadmintonacademy/'>
+                                <i class="fa-brands fa-instagram fa-3x"></i>&nbsp;
+                                <span className='label-contact'>
+                                    Instagram
+                                </span>
+                            </a>
+                        </div>
+                        <div className='actual-link'>
+                            <a className='white navlink contact-links' rel='noreferrer' target='_blank' href='https://www.facebook.com/GBATraining'>
+                                <i class="fa-brands fa-facebook fa-3x"></i>&nbsp;
+                                <span className='label-contact'>
+                                    Facebook
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='white location'>
+                        <h2>Location</h2>
+                        <img className='arena-picture' src={arena} alt=''/>
+                        <h2>
+                            2780 S Reservoir St, Pomona, CA 91766
+                        </h2>
+                    </div>
                 </div>
             </div>
         </div>
