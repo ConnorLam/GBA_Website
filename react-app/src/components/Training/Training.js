e87060e7ebe210eee5ed8e00783ec8d6d97b5f9b
Connor Lam <connorlam03@yahoo.com>
2022-12-05 16:04:54 -0800

    done working for today on training atm

--- react-app/src/components/Training/Training.js
+++ react-app/src/components/Training/Training.js
@@ -10,6 +10,9 @@ const Training = () => {
                 Intermediate, High- School and Tournament Team Training, students are 
                 always motivated and encouraged to train to their best ability.
             </div>
+            <div className='white'>
+                Little Champion
+            </div>
             <div className='schedule'>
                 <div className='row1 white'>
                     <div className='each-column'>
@@ -58,6 +61,69 @@ const Training = () => {
                         </div>
                         <div className='times'>
                             4-6 PM
+                        </div>
+                    </div>
+                    <div className='each-column'>
+                        <div className='days'>
+                            Sat
+                        </div>
+                        <div className='times'>
+                            --
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='white'>
+                Intermediate & HighSchool
+            </div>
+            <div className='schedule'>
+                <div className='row1 white'>
+                    <div className='each-column'>
+                        <div className='days'>
+                            Sun
+                        </div>
+                        <div className='times'>
+                            --
+                        </div>
+                    </div>
+                    <div className='each-column'>
+                        <div className='days'>
+                            Mon
+                        </div>
+                        <div className='times'>
+                            --
+                        </div>
+                    </div>
+                    <div className='each-column'>
+                        <div className='days'>
+                            Tues
+                        </div>
+                        <div className='times'>
+                            --
+                        </div>
+                    </div>
+                    <div className='each-column'>
+                        <div className='days'>
+                            Wed
+                        </div>
+                        <div className='times'>
+                            6-8 PM
+                        </div>
+                    </div>
+                    <div className='each-column'>
+                        <div className='days'>
+                            Thurs
+                        </div>
+                        <div className='times'>
+                            --
+                        </div>
+                    </div>
+                    <div className='each-column'>
+                        <div className='days'>
+                            Fri
+                        </div>
+                        <div className='times'>
+                            6-8 PM
                         </div>
                     </div>
                     <div className='each-column'>
