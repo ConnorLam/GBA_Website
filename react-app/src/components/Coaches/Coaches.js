fd974ecc3c893d805aaf8823af6c64e5d5bbe4b0
Connor Lam <connorlam03@yahoo.com>
2022-12-07 15:06:07 -0800

    coaches page layout

--- react-app/src/components/Coaches/Coaches.js
+++ react-app/src/components/Coaches/Coaches.js
@@ -1,10 +1,63 @@
 
+import './Coaches.css'
+import christian from '../../assets/gba_photos/Christian.jpeg'
 
 const Coaches = () => {
     return (
         <div className='main-page'>
-            <div>
-                
+            <div className="white coaches-page">
+                <div className='header'>
+                    <h1 className="coach-header">Meet our dedicated Coaches</h1>
+                </div>
+                <div className='coaches-wrap'>
+                    <div className='each-coach'>
+                        <h3 className='coach-name'>Tony Gunawan</h3>
+                        <div className='coach-pic-wrapper'>
+                            <img className='coach-pic' src='' alt=''/>
+                        </div>
+                    </div>
+                    <div className='each-coach'>
+                        <h3 className='coach-name'>Eti Gunawan</h3>
+                        <div className='coach-pic-wrapper'>
+                            <img className='coach-pic' src='' alt=''/>
+                        </div>
+                    </div>
+                    <div className='each-coach'>
+                        <h3 className='coach-name'>Kennevic Asuncion</h3>
+                        <div className='coach-pic-wrapper'>
+                            <img className='coach-pic' src='' alt=''/>
+                        </div>
+                    </div>
+                    <div className='each-coach'>
+                        <h3 className='coach-name'>Christian Christianto</h3>
+                        <div className='coach-pic-wrapper'>
+                            <img className='coach-pic' src={christian} alt=''/>
+                        </div>
+                        <ul>
+                            <li>Fact 1</li>
+                            <li>Fact 2</li>
+                            <li>Fact 3</li>
+                        </ul>
+                    </div>
+                    <div className='each-coach'>
+                        <h3 className='coach-name'>Eric Go</h3>
+                        <div className='coach-pic-wrapper'>
+                            <img className='coach-pic' src='' alt=''/>
+                        </div>
+                    </div>
+                    <div className='each-coach'>
+                        <h3 className='coach-name'>Megumi Taruno</h3>
+                        <div className='coach-pic-wrapper'>
+                            <img className='coach-pic' src='' alt=''/>
+                        </div>
+                    </div>
+                    <div className='each-coach'>
+                        <h3 className='coach-name'>Josh Hwang</h3>
+                        <div className='coach-pic-wrapper'>
+                            <img className='coach-pic' src='' alt=''/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
