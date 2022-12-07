1e84f5fbb31d88570f270605d763c2a6ce44040c
Connor Lam <connorlam03@yahoo.com>
2022-12-07 14:44:44 -0800

    rough draft of training page complete

--- react-app/src/components/Training/Training.js
+++ react-app/src/components/Training/Training.js
@@ -1,6 +1,8 @@
 
 import './Training.css'
 import littleChamp from '../../assets/gba_photos/little_champion.jpeg'
+import intermediate from '../../assets/gba_photos/intermediate.jpeg'
+import { NavLink } from 'react-router-dom'
 
 const Training = () => {
 
@@ -9,10 +11,11 @@ const Training = () => {
             <div className='training-desc white'>
                 With different level of training to choose from: Little Champion, 
                 Intermediate, High- School and Tournament Team Training, students are 
-                always motivated and encouraged to train to their best ability.
+                always motivated and encouraged to train to their best ability. If you would like to join
+                the tournament team please <span><NavLink to='/contact' className='navlink-blue'>contact us</NavLink></span> to book an appointment.
             </div>
 
-            <div className='whole-schedule-first'>
+            <div className='whole-schedule'>
                 <div className='white header-section'>
                     <div className='schedule-header'>
                         <h3>
@@ -88,65 +91,77 @@ const Training = () => {
                     </div>
                 </div>
             </div>
-            <div className='white'>
-                INTERMEDIATE & HIGHSCHOOL
-            </div>
-            <div className='schedule'>
-                <div className='row1 white'>
-                    <div className='each-column'>
-                        <div className='days'>
-                            Sun
-                        </div>
-                        <div className='times'>
-                            ---
+            <div className='whole-schedule'>
+                <div className='white header-section'>
+                    <div className='schedule-header'>
+                        <h3>
+                            INTERMEDIATE & HIGHSCHOOL
+                        </h3>
+                        <div className='grey'>
+                            Intermediate & Highschool is our program for kids 
                         </div>
                     </div>
-                    <div className='each-column'>
-                        <div className='days'>
-                            Mon
-                        </div>
-                        <div className='times'>
-                            ---
-                        </div>
+                    <div className='training-photo'>
+                        <img className='training-actual-photo' src={intermediate} alt=''/>
                     </div>
-                    <div className='each-column'>
-                        <div className='days'>
-                            Tues
+                </div>
+                <div className='schedule'>
+                    <div className='row1 white'>
+                        <div className='each-column'>
+                            <div className='days'>
+                                Sun
+                            </div>
+                            <div className='times'>
+                                ---
+                            </div>
                         </div>
-                        <div className='times'>
-                            ---
+                        <div className='each-column'>
+                            <div className='days'>
+                                Mon
+                            </div>
+                            <div className='times'>
+                                ---
+                            </div>
                         </div>
-                    </div>
-                    <div className='each-column'>
-                        <div className='days'>
-                            Wed
+                        <div className='each-column'>
+                            <div className='days'>
+                                Tues
+                            </div>
+                            <div className='times'>
+                                ---
+                            </div>
                         </div>
-                        <div className='times'>
-                            6-8 PM
+                        <div className='each-column'>
+                            <div className='days'>
+                                Wed
+                            </div>
+                            <div className='times'>
+                                6-8 PM
+                            </div>
                         </div>
-                    </div>
-                    <div className='each-column'>
-                        <div className='days'>
-                            Thurs
+                        <div className='each-column'>
+                            <div className='days'>
+                                Thurs
+                            </div>
+                            <div className='times'>
+                                ---
+                            </div>
                         </div>
-                        <div className='times'>
-                            ---
+                        <div className='each-column'>
+                            <div className='days'>
+                                Fri
+                            </div>
+                            <div className='times'>
+                                6-8 PM
+                            </div>
                         </div>
-                    </div>
-                    <div className='each-column'>
-                        <div className='days'>
-                            Fri
-                        </div>
-                        <div className='times'>
-                            6-8 PM
-                        </div>
-                    </div>
-                    <div className='each-column'>
-                        <div className='days'>
-                            Sat
-                        </div>
-                        <div className='times'>
-                            ---
+                        <div className='each-column'>
+                            <div className='days'>
+                                Sat
+                            </div>
+                            <div className='times'>
+                                ---
+                            </div>
                         </div>
                     </div>
                 </div>
