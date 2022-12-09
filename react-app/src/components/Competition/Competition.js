ffb1a78757d3a5a1b99c0dd7df19905a57372728
Connor Lam <connorlam03@yahoo.com>
2022-12-08 18:02:42 -0800

    done working for now

--- react-app/src/components/Competition/Competition.js
+++ react-app/src/components/Competition/Competition.js
@@ -1,9 +1,18 @@
+import './Competition.css'
+
 
 const Competition = () => {
 
     return (
         <div className="main-page">
-
+            <div className="white">
+                <div className="competition-header">
+                    Here at GBA we pride ourselves of bringing the most we 
+                    can out of our students and we have the accolades to prove it. With every tournament our kids fight and make it to the top of the podium.
+                    If you want to be a competitive badminton player than we will do what we can
+                    to bring you to the top.
+                </div>
+            </div>
         </div>
     )
 }
