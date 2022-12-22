f01a8a1815b5d8a1bca04fa2dc0c0c53f5c61f3f
Connor Lam <connorlam03@yahoo.com>
2022-12-22 12:35:14 -0800

    adding post seeders

--- react-app/src/components/Updates/Updates.js
+++ react-app/src/components/Updates/Updates.js
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux"
+import { getPostThunk } from "../../store/post"
+
+
 
 const Updates = () => {
+
+    const state = useSelector(state => state)
+    console.log(state)
+
     return (
         <div className="main-page">
             <div className="white">
