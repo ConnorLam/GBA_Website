a6110a18bd23a43dffd8b59cfc1523ae015781b3
Connor Lam <connorlam03@yahoo.com>
2023-01-04 13:13:49 -0800

    worked a little bit on project

--- react-app/src/components/Updates/UpdatesList/UpdateCard.js
+++ react-app/src/components/Updates/UpdatesList/UpdateCard.js
@@ -1,9 +1,20 @@
 
 
 const UpdateCard = ({post}) => {
-    console.log(post)
+    // console.log(post)
+
+    const newDate = (post) => {
+        // console.log(Date(post.timeCreate))
+        let newDateObj = new Date(post.timeCreate)
+        // console.log(newDateObj)
+        return newDateObj.toLocaleDateString();
+    }
+
     return (
         <div>
+            <div>
+                {newDate(post)}
+            </div>
             <div>
                 {post.owner.firstName} {post.owner.lastName}
             </div>
