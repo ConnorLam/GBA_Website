516ce7f4906ec8cf4717e807d261231c7fb5b891
Connor Lam <connorlam03@yahoo.com>
2023-01-07 16:36:29 -0800

    starting post updates page

---
+++ react-app/src/components/Updates/CreateUpdate/CreateUpdate.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useHistory, Redirect } from "react-router-dom";
+
+
+const CreateUpdate = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
