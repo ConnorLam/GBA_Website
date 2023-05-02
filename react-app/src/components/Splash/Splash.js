bc5d7562cfd8a420686379dbcaf925e532f986ae
Connor Lam <connorlam03@yahoo.com>
2023-05-02 14:39:33 -0700

    took out the _next from form submit to see what problem is

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -47,57 +47,57 @@ const Splash = () => {
     //       <h1>Global Badminton Academy</h1>
     //     </div> */}
     //   </div>
-    // <div className="splash-wrapper">
-    //     <div className="splash-info">
-    //         <div>
-    //             <h1>
-    //                 Welcome to Global Badminton Academy
-    //             </h1>
-    //         </div>
-    //         <div className="coach-intro">
-    //             Tony Gunawan, <span className="x-large-font">Olympic Gold Medalist</span>, founded Global Badminton Academy 
-    //             in 2010. His mission is to share his profound knowledge through intensive training 
-    //             and world-class competition to as many badminton coaches and students 
-    //             throughout the US.
-    //         </div>
-    //     </div>
-    //     <div className="splash-picture">
-    //         <img className='picture' src={tony} alt=''/>
-    //     </div>
-    // </div>
-    <div className="main-splash">
-
-        <div className="four-quadrants white">
-            <div className="top-bottom-quad">
-                <div className="each-quad">
-                    <div className="quad-header">
-                        Little Champions
-                    </div>
-                    <img src="" alt=""/>
-                </div >
-                <div className="each-quad">
-                    <div className="quad-header">
-                        Tournament Team
-                    </div>
-                    <img className='background-image' src={splash1} alt=""/>
-                </div>
+    <div className="splash-wrapper">
+        <div className="splash-info">
+            <div>
+                <h1>
+                    Welcome to Global Badminton Academy
+                </h1>
             </div>
-            <div className="top-bottom-quad">
-                <div className="each-quad">
-                    <div className="quad-header">
-                        Our Coaches
-                    </div>
-                    <img src="" alt=""/>
-                </div>
-                <div className="each-quad">
-                    <div className="quad-header">
-                        FAQ
-                    </div>
-                    <img src="" alt=""/>
-                </div>
+            <div className="coach-intro">
+                Tony Gunawan, <span className="x-large-font">Olympic Gold Medalist</span>, founded Global Badminton Academy 
+                in 2010. His mission is to share his profound knowledge through intensive training 
+                and world-class competition to as many badminton coaches and students 
+                throughout the US.
             </div>
         </div>
+        <div className="splash-picture">
+            <img className='picture' src={tony} alt=''/>
+        </div>
     </div>
+    // <div className="main-splash">
+
+    //     <div className="four-quadrants white">
+    //         <div className="top-bottom-quad">
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     Little Champions
+    //                 </div>
+    //                 <img src="" alt=""/>
+    //             </div >
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     Tournament Team
+    //                 </div>
+    //                 <img className='background-image' src={splash1} alt=""/>
+    //             </div>
+    //         </div>
+    //         <div className="top-bottom-quad">
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     Our Coaches
+    //                 </div>
+    //                 <img src="" alt=""/>
+    //             </div>
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     FAQ
+    //                 </div>
+    //                 <img src="" alt=""/>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
     );
 }
 
