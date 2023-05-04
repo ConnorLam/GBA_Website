ecbea4a817856db80825cb1b745c9865ab520be8
Connor Lam <connorlam03@yahoo.com>
2023-05-04 14:34:18 -0700

    make an image carousel for splash page next

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
-import './Splash.css'
+import './Splash.css';
 
 import splash1 from '../../assets/gba_photos/gba_splash.jpeg'
 import splash2 from '../../assets/gba_photos/gba_splash2.jpeg'
@@ -12,11 +12,10 @@ import coaches from '../../assets/gba_photos/coaches.JPG'
 import tournamentteam from '../../assets/gba_photos/tournamentteam.JPG'
 import littlechampion from '../../assets/gba_photos/little_champion2.jpeg'
 
-// const images = [
-//   splash1,
-//   splash2,
-//   splash3
-// ];
+const images = [
+  tournamentteam,
+  littlechampion
+];
 
 const Splash = () => {
 
@@ -26,29 +25,31 @@ const Splash = () => {
     const [splashNum, setSplashNum] = useState(0);
     const [isLoaded, setIsLoaded] = useState(false);
 
-    // useEffect(() => {
-    //     const splashInterval = setInterval(() => {
-    //         setSplashNum((prevNum) => ++prevNum % images.length)
-    //     }, 5000)
+    useEffect(() => {
+        const splashInterval = setInterval(() => {
+            setSplashNum((splashNum + 1) % images.length)
+        }, 5000)
 
-    //     return (() => {
-    //         clearInterval(splashInterval)
-    //     })
-    // }, [])
+        return (() => {
+            clearInterval(splashInterval)
+        })
+    }, [splashNum])
 
     return (
-    //   <div
-    //     id="splash"
-    //     className="splash-main"
-    //     style={{
-    //       backgroundImage: `url(${images[splashNum]})`,
-    //     }}
-    //   >
-    //     {/* <img src={images[splashNum]} alt=''/> */}
-    //     {/* <div>
-    //       <h1>Global Badminton Academy</h1>
-    //     </div> */}
-    //   </div>
+        <div className="splash-wrapper">
+            <div
+                id="splash"
+                className="splash-main"
+                // style={{
+                // backgroundImage: `url(${images[splashNum]})`,
+                // }}
+            >
+                <img className='rotating-image' src={images[splashNum]} alt=''/>
+                {/* <div>
+                <h1>Global Badminton Academy</h1>
+                </div> */}
+            </div>
+        </div>
     // <div className="splash-wrapper">
     //     <div className="splash-info">
     //         <div>
@@ -67,41 +68,42 @@ const Splash = () => {
     //         <img className='picture' src={tony} alt=''/>
     //     </div>
     // </div>
-    <div className="main-splash">
+    // <div className="main-splash">
 
-        <div className="four-quadrants white">
-            <div className="top-bottom-quad">
-                <div className="each-quad">
-                    <div className="quad-header">
-                        Little Champions
-                    </div>
-                    <img className="background-image" id="little-champions" src={littlechampion} alt=""/>
-                </div >
-                <div className="each-quad">
-                    <div className="quad-header">
-                        Tournament Team
-                    </div>
-                    <img className='background-image' id="tournament-team" src={tournamentteam} alt=""/>
-                </div>
-            </div>
-            <div className="top-bottom-quad">
-                <div className="each-quad">
-                    <div className="quad-header">
-                        Our Coaches
-                    </div>
-                    <img className="background-image" id="coaches" src={coaches} alt=""/>
-                </div>
-                <div className="each-quad">
-                    <div className="quad-header">
-                        FAQ
-                    </div>
-                    <img src="" alt=""/>
-                </div>
-            </div>
-        </div>
-    </div>
+    //     <div className="four-quadrants white">
+    //         <div className="top-bottom-quad">
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     Little Champions
+    //                 </div>
+    //                 <img className="background-image" id="little-champions" src={littlechampion} alt=""/>
+    //             </div >
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     Tournament Team
+    //                 </div>
+    //                 <img className='background-image' id="tournament-team" src={tournamentteam} alt=""/>
+    //             </div>
+    //         </div>
+    //         <div className="top-bottom-quad">
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     Our Coaches
+    //                 </div>
+    //                 <img className="background-image" id="coaches" src={coaches} alt=""/>
+    //             </div>
+    //             <div className="each-quad">
+    //                 <div className="quad-header">
+    //                     FAQ
+    //                 </div>
+    //                 <img src="" alt=""/>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
     );
 }
+
 
 
 export default Splash
