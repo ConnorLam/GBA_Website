57b8168ff9b330a1fc59bd7cb14969d45d632460
Connor Lam <connorlam03@yahoo.com>
2023-05-24 13:39:22 -0700

    added camp jot form into site

--- react-app/src/components/Camp/Camp.js
+++ react-app/src/components/Camp/Camp.js
@@ -7,12 +7,18 @@ const Camp = () => {
       <div className="main-page">
         <div className="white">
             <div className="camp-explanation">
-                Every summer and winter, we are hosting fun camp for many students.
-                Summer Camp starts as early as first week of June until mid August.
-                Winter Camp starts during school winter break.
+                Ready to level up your badminton game? Join our summer and/or winter badminton camp for an intensive training
+                experience. Led by expert coaches and professional players, our program focuses on technique, strategy, 
+                conditioning, and mental preparation. Train alongside passionate players, develop friendships, 
+                and enhance your skills in a supportive environment. Don't miss out on this incredible opportunity to take 
+                your badminton to new heights. Secure your spot today and get ready for a summer/winter of growth and fun on 
+                the court!
             </div>
-            <div className='camp-img-wrap'>
+            {/* <div className='camp-img-wrap'>
                 <img src={camp} alt='' />
+            </div> */}
+            <div className='jotform-div'>
+              <embed className='jotform' title='iframetest' width='300' height='200' src='https://form.jotform.com/231193271833151' ></embed>
             </div>
         </div>
       </div>
