f9898807215ac7e8037930ade74177d2a0188084
Connor Lam <connorlam03@yahoo.com>
2023-05-25 14:56:34 -0700

    made last fixes for logo and navbar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import { useSelector } from 'react-redux';
-import gba_icon from '../../assets/favicon_io (1)/favicon-32x32.png'
+// import gba_icon from '../../assets/favicon_io (1)/favicon-32x32.png'
 import './NavBar.css'
 import ProfileButton from './ProfileButton';
 import { useLocation } from 'react-router-dom';
-import gba_logo from '../../assets/gba_photos/gba_logo.gif'
+import gba_logo from '../../assets/gba_photos/6.png'
 
 const NavBar = () => {
 
@@ -45,25 +45,33 @@ const NavBar = () => {
             <NavLink to='/' exact={true} activeClassName='active' className='navlink'>
               <div className='home-button-div'>
                 {/* <span>
-                  <img alt='' src={gba_icon}/>
+                  <img className='gba-logo' alt='' src={gba_icon}/>
                 </span> */}
-                {/* <img src={gba_logo} alt=''/> */}
-                GBA 🏸
+                <img className='gba-logo' src={gba_logo} alt=''/>
+                {/* GBA 🏸 */}
               </div>
             </NavLink>
         </div>
-        {session}
+          <NavLink exact to='/' activeClassName='active' className='navlink login-signup-link'>Home</NavLink>
+          <NavLink to='/training' activeClassName='active' className='navlink login-signup-link'>Training</NavLink>
+          <NavLink to='/camp' activeClassName='active' className='navlink login-signup-link'>Camp</NavLink>
+          <NavLink to='/competition' activeClassName='active' className='navlink login-signup-link'>Competition</NavLink>
+          <NavLink to='/coaches' activeClassName='active' className='navlink login-signup-link'>Coaches</NavLink>
+          {/* <NavLink to='/gallery' activeClassName='active' className='navlink login-signup-link'>Photos</NavLink> */}
+          <NavLink to='/updates' activeClassName='active' className='navlink login-signup-link'>Updates</NavLink>
+          <NavLink to='/contact' activeClassName='active' className='navlink login-signup-link'>Contact Us</NavLink>
+          {session}
       </nav>
-      <div className='whole-bottom-nav' id={location.pathname === '/login' || location.pathname === '/sign-up' ? 'display-none' : null}>
+      {/* <div className='whole-bottom-nav' id={location.pathname === '/login' || location.pathname === '/sign-up' ? 'display-none' : null}>
         <NavLink exact to='/' activeClassName='active' className='navlink login-signup-link'>Home</NavLink>
         <NavLink to='/training' activeClassName='active' className='navlink login-signup-link'>Training</NavLink>
         <NavLink to='/camp' activeClassName='active' className='navlink login-signup-link'>Camp</NavLink>
         <NavLink to='/competition' activeClassName='active' className='navlink login-signup-link'>Competition</NavLink>
         <NavLink to='/coaches' activeClassName='active' className='navlink login-signup-link'>Coaches</NavLink>
-        {/* <NavLink to='/gallery' activeClassName='active' className='navlink login-signup-link'>Photos</NavLink> */}
+        <NavLink to='/gallery' activeClassName='active' className='navlink login-signup-link'>Photos</NavLink>
         <NavLink to='/updates' activeClassName='active' className='navlink login-signup-link'>Updates</NavLink>
         <NavLink to='/contact' activeClassName='active' className='navlink login-signup-link'>Contact Us</NavLink>
-      </div>
+      </div> */}
     </div>
   );
 }
