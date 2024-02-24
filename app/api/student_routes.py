958fd1e997916bad0d977991c30ac78c9565ee9e
Connor Lam <connorlam03@yahoo.com>
2024-02-23 23:14:45 -0800

    finished student routes on the backend, have to decide if i want groups to be a different table so it can be potentially easier to categorize later

--- app/api/student_routes.py
+++ app/api/student_routes.py
@@ -22,7 +22,7 @@ def create_student():
 
     if not current_user:
         return {"message": "No user", "statusCode": 403}, 403
-    
+
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         student = Student(
@@ -37,5 +37,42 @@ def create_student():
         db.session.commit()
 
         return student.to_dict()
-    
+
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+@student_routes.route('<int:id>', methods=['PUT'])
+@login_required
+def edit_student(id):
+    student = Student.query.get(id)
+
+    if not student:
+        return {"message": "Student could not be found", "statusCode": 404}, 404
+
+    form = CreateStudentForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        student.first_name = form.firstName.data
+        student.last_name = form.lastName.data
+        student.email = form.email.data
+        student.parent_name = form.parentName.data
+        student.parent_email = form.parentEmail.data
+        student.group = form.group.data
+
+        db.session.commit()
+
+        return student.to_dict()
+
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+@student_routes.route('<int:id>', methods=['DELETE'])
+def delete_student(id):
+    student = Student.query.get(id)
+
+    if not student:
+        return {'message': "Student could not be found", 'statusCode': 404}, 404
+
+    db.session.delete(student)
+    db.session.commit()
+
+    return {"message": "Succesfully deleted", "statusCode": 200}, 200
